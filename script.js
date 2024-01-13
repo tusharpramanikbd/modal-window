4da88d7f70e23a07ff2c8504add3a421a5b9af17
Tushar Pramanik <tushar.pramanik@lastingdynamics.com>
2024-01-13 20:29:19 +0600

    implemented btn click events

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
+
+const handleBtnClick = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const handleCloseModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsShowModal.forEach(btn => {
+  btn.addEventListener('click', handleBtnClick);
+});
+
+btnCloseModal.addEventListener('click', handleCloseModal);
+
+overlay.addEventListener('click', handleCloseModal);
